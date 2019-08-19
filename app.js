370d00c1121f9ca626c894355dddd495dabda015
osama-moussa <osamamamoussa@gmail.com>
2019-08-19 04:26:59 +0200

    added

---
+++ app.js
@@ -0,0 +1,84 @@
+class Book {
+    constructor (title,author,isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+class UI {
+  static displayBook(){
+      const StoredBooks = [ ];
+        const Books = StoredBooks;
+
+        Books.forEach((book)=> UI.addBookToList(book));
+        }     
+        
+        
+        static addBookToList(book){
+          const list= document.querySelector("#book-list");
+
+          const row  = document.createElement("tr");
+              row.innerHTML = `
+              <td>${book.title}</td>
+              <td>${book.author}</td>
+              <td>${book.isbn}</td>
+              <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+            `;
+            
+          list.appendChild(row);
+        }
+
+        static deleteBook(el) {
+            if(el.classList.contains('delete'))
+            {
+              el.parentElement.parentElement.remove();
+            }
+        }
+
+        static showAlert(message, className) {    
+          const div = document.createElement('div');   
+          div.className = `alert alert-${className}`;  
+          div.appendChild(document.createTextNode(message));   
+          const container = document.querySelector('.container');  
+          const form = document.querySelector('#book-form'); 
+          container.insertBefore(div, form); 
+      
+          
+          setTimeout(() => document.querySelector('.alert').remove(), 3000);
+        }
+        
+        static clearfields(){
+             document.querySelector('#title').value ='';
+             document.querySelector('#author').value="";
+             document.querySelector('#isbn').value ="";
+
+        }        
+
+}
+
+       
+        document.addEventListener('DOMContentLoaded',UI.displayBook);
+
+        document.querySelector('#book-form').addEventListener('submit',(e)=>{
+            e.preventDefault();
+            const title  = document.querySelector('#title').value;
+            const author = document.querySelector('#author').value;
+            const isbn   = document.querySelector('#isbn').value;
+            
+            if(title === '' || author === '' || isbn === '') {
+              UI.showAlert('Please fill in all fields', 'danger');
+
+            }else{
+
+              const book = new Book (title,author,isbn);
+              UI.addBookToList(book);
+              UI.clearfields();
+              UI.showAlert('book added','success')
+            }
+        });
+
+        document.querySelector('#book-list').addEventListener('click',(e)=>{
+          UI.deleteBook(e.target);
+          UI.showAlert('Book Removed','warning');
+        });
